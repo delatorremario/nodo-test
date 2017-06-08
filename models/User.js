08a719ee1909f82ede5eecf55464aa1126c42cea
Mario de la Torre <mariodelatorre@MacBook-Pro-de-MacPro.local>
2017-06-08 15:41:15 -0300

    1. Modelo de Usuario

--- models/User.js
+++ models/User.js
@@ -2,9 +2,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var UserSchema   = new Schema({
-	name: String
-	/* TODO: Agregar los campos necesarios para el funcionamiento de la app */	
+var UserSchema = new Schema({
+	name: String,
+	email: String,
+	gender: String,
+	"location.street": String,
+	"location.city": String,
+	"location.state": String,
+	"location.postcode": String,
+	"picture.large": String,
+	"picture.medium": String,
+	"picture.thumbnail": String
 });
 
 /* TODO: Métodos de clase */
